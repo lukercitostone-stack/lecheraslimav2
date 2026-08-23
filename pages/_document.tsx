ba98279eeda0203792e08d5379c6b9a813514b76
solucionessoftware2000 <soluciones.software.2000@gmail.com>
2026-08-23 15:23:21 -0500

    fix: contain Adsterra ad units to prevent horizontal layout overflow

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,18 +19,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `atOptions = {
-  'key' : '466bc8840ab56a9dbd48b7de1970306c',
-  'format' : 'iframe',
-  'height' : 60,
-  'width' : 468,
-  'params' : {}
-};`,
-          }}
-        />
-        <script src="https://www.highrevenueformat.com/466bc8840ab56a9dbd48b7de1970306c/invoke.js" />
         <script src="https://pl30997683.profitableratecpmnetwork.com/8a/26/0c/8a260c1e4a39411cfd81c8720cccdb29.js" />
         <script src="https://pl30997684.profitableratecpmnetwork.com/26/19/aa/2619aa80fd5c3d81691563afe76c08ca.js" />
         <script
@@ -38,7 +26,31 @@ export default function Document() {
           data-cfasync="false"
           src="https://pl30997799.profitableratecpmnetwork.com/1f02e1385e7aaef96ad95036e75f4be5/invoke.js"
         />
-        <div id="container-1f02e1385e7aaef96ad95036e75f4be5" />
+        <div
+          style={{
+            maxWidth: "100%",
+            width: "468px",
+            margin: "0 auto",
+            overflow: "hidden",
+          }}
+        >
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `atOptions = {
+  'key' : '466bc8840ab56a9dbd48b7de1970306c',
+  'format' : 'iframe',
+  'height' : 60,
+  'width' : 468,
+  'params' : {}
+};`,
+            }}
+          />
+          <script src="https://www.highrevenueformat.com/466bc8840ab56a9dbd48b7de1970306c/invoke.js" />
+        </div>
+        <div
+          id="container-1f02e1385e7aaef96ad95036e75f4be5"
+          style={{ maxWidth: "100%", overflow: "hidden", margin: "0 auto" }}
+        />
       </body>
     </Html>
   );
