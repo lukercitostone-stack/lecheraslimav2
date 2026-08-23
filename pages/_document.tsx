20cd2092b0e057448b1c6e0c2caeac4c4a352063
solucionessoftware2000 <soluciones.software.2000@gmail.com>
2026-08-23 15:21:59 -0500

    feat: add Adsterra native banner script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,6 +33,12 @@ export default function Document() {
         <script src="https://www.highrevenueformat.com/466bc8840ab56a9dbd48b7de1970306c/invoke.js" />
         <script src="https://pl30997683.profitableratecpmnetwork.com/8a/26/0c/8a260c1e4a39411cfd81c8720cccdb29.js" />
         <script src="https://pl30997684.profitableratecpmnetwork.com/26/19/aa/2619aa80fd5c3d81691563afe76c08ca.js" />
+        <script
+          async
+          data-cfasync="false"
+          src="https://pl30997799.profitableratecpmnetwork.com/1f02e1385e7aaef96ad95036e75f4be5/invoke.js"
+        />
+        <div id="container-1f02e1385e7aaef96ad95036e75f4be5" />
       </body>
     </Html>
   );
