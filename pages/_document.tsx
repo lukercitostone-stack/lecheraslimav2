e82d9ecaf61533d0fb6af77371826a825407fd1b
solucionessoftware2000 <soluciones.software.2000@gmail.com>
2026-08-23 15:17:53 -0500

    feat: add Adsterra ad network scripts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,20 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `atOptions = {
+  'key' : '466bc8840ab56a9dbd48b7de1970306c',
+  'format' : 'iframe',
+  'height' : 60,
+  'width' : 468,
+  'params' : {}
+};`,
+          }}
+        />
+        <script src="https://www.highrevenueformat.com/466bc8840ab56a9dbd48b7de1970306c/invoke.js" />
+        <script src="https://pl30997683.profitableratecpmnetwork.com/8a/26/0c/8a260c1e4a39411cfd81c8720cccdb29.js" />
+        <script src="https://pl30997684.profitableratecpmnetwork.com/26/19/aa/2619aa80fd5c3d81691563afe76c08ca.js" />
       </body>
     </Html>
   );
